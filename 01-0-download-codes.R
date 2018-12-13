ff083843fd190b425ec3b80ecafe11b580f434d9
pachamaltese <mvargas@dcc.uchile.cl>
2018-12-13 14:27:06 -0300

    adds GNU terms to all scripts

--- 01-0-download-codes.R
+++ 01-0-download-codes.R
@@ -1,37 +1,54 @@
 # Open ts-comtrade-codes.Rproj before running this script
 
+# Copyright (c) 2018, Mauricio \"Pacha\" Vargas
+# This file is part of Open Trade Statistics project
+# The scripts within this project are released under GNU General Public License 3.0
+# See https://github.com/tradestatistics/ts-yearly-datasets/LICENSE for the details
+
 #1: Import country codes
 
 raw_country_dir <- "01-1-raw-country-data"
 try(dir.create(raw_country_dir))
 
-url <- "https://comtrade.un.org/data/cache/reporterAreas.json"
-json <- paste0(raw_country_dir, "/country-codes.json")
-if (!file.exists(json)) {
-  download.file(url, json, method = "wget")
-}
+url <- c(
+  "https://comtrade.un.org/data/cache/reporterAreas.json",
+  "http://unstats.un.org/unsd/tradekb/Attachment321.aspx?AttachmentType=1"
+)
 
-url <- "http://unstats.un.org/unsd/tradekb/Attachment321.aspx?AttachmentType=1"
-xls <- paste0(raw_country_dir, "/country-codes.xls")
-if (!file.exists(xls)) {
-  download.file(url, xls, method = "wget")
-}
+file <- paste0(raw_country_dir, 
+                c(
+                  "/country-codes.json",
+                  "/country-codes.xls"
+                  )
+)
+
+lapply(seq_along(url), function (t) if (!file.exists(file[t])) { download.file(url[t], file[t]) })
 
 #2: Import product codes
 
 raw_product_dir <- "02-1-raw-product-data"
 try(dir.create(raw_product_dir))
 
-url <- "https://unstats.un.org/unsd/tradekb/Attachment439.aspx?AttachmentType=1"
-xlsx <- paste0(raw_product_dir, "/official-list-of-comtrade-codes.xlsx")
+url <- c(
+  "https://unstats.un.org/unsd/tradekb/Attachment439.aspx?AttachmentType=1",
+  "https://unstats.un.org/unsd/trade/classifications/tables/CompleteCorrelationsOfHS-SITC-BEC_20170606.xlsx",
+  "https://unstats.un.org/unsd/trade/classifications/tables/HS%202007%20to%20SITC%20Rev%201%20Correlation%20and%20conversion%20tables.xls",
+  "https://unstats.un.org/unsd/trade/classifications/tables/HS%202007%20to%20SITC%20Rev%202%20Correlation%20and%20conversion%20tables.xls",
+  "https://unstats.un.org/unsd/trade/classifications/tables/HS%202007%20to%20HS%201992%20Correlation%20and%20conversion%20tables.xls",
+  "https://unstats.un.org/unsd/trade/classifications/tables/HS%202007%20to%20HS%201996%20Correlation%20and%20conversion%20tables.xls",
+  "https://unstats.un.org/unsd/trade/classifications/tables/HS%202007%20to%20HS%202002%20Correlation%20and%20conversion%20tables.xls"
+)
+  
+file <- paste0(raw_product_dir, 
+               c(
+                 "/official-list-of-comtrade-codes.xlsx",
+                 "/official-correlations-between-comtrade-codes.xlsx",
+                 "/hs2007-to-sitc1.xls",
+                 "/hs2007-to-sitc2.xls",
+                 "/hs2007-to-hs1992.xls",
+                 "/hs2007-to-hs1996.xls",
+                 "/hs2007-to-hs2002.xls"
+                )
+)
 
-if (!file.exists(xlsx)) {
-  download.file(url, xlsx)
-}
-
-url <- "https://unstats.un.org/unsd/trade/classifications/tables/CompleteCorrelationsOfHS-SITC-BEC_20170606.xlsx"
-xlsx <- paste0(raw_product_dir, "/official-correspondence-between-comtrade-codes.xlsx")
-
-if (!file.exists(xlsx)) {
-  download.file(url, xlsx)
-}
+lapply(seq_along(url), function (t) if (!file.exists(file[t])) { download.file(url[t], file[t]) })
--- 02-0-tidy-codes.R
+++ 02-0-tidy-codes.R
@@ -1,5 +1,10 @@
 # Open ts-comtrade-codes.Rproj before running this script
 
+# Copyright (c) 2018, Mauricio \"Pacha\" Vargas
+# This file is part of Open Trade Statistics project
+# The scripts within this project are released under GNU General Public License 3.0
+# See https://github.com/tradestatistics/ts-yearly-datasets/LICENSE for the details
+
 #1: Tidy country codes
 
 if (!require("pacman")) install.packages("pacman")
@@ -25,7 +30,35 @@ product_codes <- read_excel(paste0(raw_product_dir, "/official-list-of-comtrade-
 
 save(product_codes, file = paste0(tidy_product_dir, "/product-codes.RData"), compress = "xz")
 
-product_correspondence <- read_excel(paste0(raw_product_dir, "/official-correspondence-between-comtrade-codes.xlsx")) %>% 
+product_correlation <- read_excel(paste0(raw_product_dir, "/official-correlations-between-comtrade-codes.xlsx")) %>% 
   clean_names()
 
-save(product_correspondence, file = paste0(tidy_product_dir, "/product-correspondence.RData"), compress = "xz")
+save(product_correlation, file = paste0(tidy_product_dir, "/product-correlation.RData"), compress = "xz")
+
+hs2007_to_sitc1 <- read_excel("02-1-raw-product-data/hs2007-to-sitc1.xls", sheet = "Conversion Table", range = "A1:B5046") %>% 
+  rename(hs07 = From, sitc1 = To) %>% 
+  filter(row_number() > 1)
+
+hs2007_to_sitc2 <- read_excel("02-1-raw-product-data/hs2007-to-sitc2.xls", sheet = "Conversion Table", range = "B1:C5052") %>% 
+  rename(hs07 = From, sitc2 = To) %>% 
+  filter(row_number() > 1)
+
+hs2007_to_hs1992 <- read_excel("02-1-raw-product-data/hs2007-to-hs1992.xls",  sheet = "Conversion Tables", range = "A1:B5054") %>% 
+  rename(hs07 = From, hs92 = To) %>% 
+  filter(row_number() > 1)
+
+hs2007_to_hs1996 <- read_excel("02-1-raw-product-data/hs2007-to-hs1996.xls", sheet = "Conversion Tables", range = "A1:B5054") %>% 
+  rename(hs07 = From, hs96 = To) %>% 
+  filter(row_number() > 1)
+
+hs2007_to_hs2002 <- read_excel("02-1-raw-product-data/hs2007-to-hs2002.xls", sheet = "Conversion Tables", range = "A1:B5054") %>% 
+  rename(hs07 = From, hs02 = To) %>% 
+  filter(row_number() > 1)
+
+product_conversion <- hs2007_to_hs1992 %>% 
+  full_join(hs2007_to_hs1996) %>% 
+  full_join(hs2007_to_hs2002) %>% 
+  full_join(hs2007_to_sitc1) %>% 
+  full_join(hs2007_to_sitc2)
+
+save(product_conversion, file = paste0(tidy_product_dir, "/product-conversion.RData"), compress = "xz")
